80aa5cc12abd746a3a09bc31582d673b1d1b1ebc
Hitoshi AFUSO <hitoshiafuso@gmail.com>
2025-09-20 11:06:42 +0900

    Initial Commit

---
+++ lolupReactionTestApp/lolupReactionTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  lolupReactionTestAppApp.swift
+//  lolupReactionTestApp
+//
+//  Created by Hitoshi AFUSO on 2025/09/20.
+//
+
+import SwiftUI
+
+@main
+struct lolupReactionTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
